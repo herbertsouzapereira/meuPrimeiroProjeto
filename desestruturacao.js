fae6e35425e8d4ee29934157d494a3a2881f2cd4
herbertsouzapereira <herbert.souza.pereira@gmail.com>
2020-11-23 22:15:58 -0300

    Meu primeiro commitgit status

---
+++ desestruturacao.js
@@ -0,0 +1,20 @@
+/* let pessoa ={
+
+    nome: "Herbert",
+    altura: 1.80
+}
+
+console.log (pessoa.nome)
+
+/* const {nome, altura} = pessoa
+
+console.log (altura) */
+
+
+let listaCompras = ["pao","leite","ovos",'nescau']
+
+console.log (listaCompras[3])
+
+const [item0, , item1] = listaCompras
+
+console.log (item1)
